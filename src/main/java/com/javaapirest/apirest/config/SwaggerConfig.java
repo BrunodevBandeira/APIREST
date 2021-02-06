9ae6a65598b79ad36c61b2d872b729abba7330b2
Bruno Bandeira <brunodev29ab@gmail.com>
2021-02-06 17:26:58 -0300

    Adicionando o banco de dados H2

--- src/main/java/com/javaapirest/apirest/config/SwaggerConfig.java
+++
@@ -1,5 +0,0 @@
-package com.javaapirest.apirest.config;
-
-public class SwaggerConfig {
-
-}
---
+++ src/main/java/com/javaapirest/apirest/config/SwaggerConfig_A.java
@@ -0,0 +1,21 @@
+package com.javaapirest.apirest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@EnableSwagger2
+public class SwaggerConfig_A {
+	
+	@Bean
+	public Docket productApi() {
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
